8bbaf6ae2f0f7a90f767e6a199c0a5618e075c41
francitonioferreiraalves <francitonio@gmail.com>
2022-10-30 13:00:37 -0300

    criação das classes para teste rabbitMQ

---
+++ order-service/order-service/src/main/java/com/example/springmqp/aula1/Order.java
@@ -0,0 +1,47 @@
+package com.example.springmqp.aula1;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_order")
+public class Order {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private BigDecimal value = BigDecimal.ZERO;
+	private Boolean paid = Boolean.FALSE;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public BigDecimal getValue() {
+		return value;
+	}
+
+	public void setValue(BigDecimal value) {
+		this.value = value;
+	}
+
+	public boolean isPaid() {
+		return paid;
+	}
+
+	public void setPaid(boolean paid) {
+		this.paid = paid;
+	}
+
+	public void markAsPaid() {
+		this.paid = true;
+	}
+}
---
+++ order-service/order-service/src/test/java/com/example/springmqp/aula1/OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springmqp.aula1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
